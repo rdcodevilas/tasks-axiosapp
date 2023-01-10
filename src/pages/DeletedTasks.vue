8a56848ea09ce036bd0f0fb6faff99e66db12acc
Ralph Odevilas <rdcodevilas@gmail.com>
2023-01-11 03:19:18 +0800

    Today Commit

--- src/pages/DeletedTasks.vue
+++ src/pages/DeletedTasks.vue
@@ -8,7 +8,7 @@ import { DeletedTasks } from "../composables/Tasks";
 <template>
   <div class="flex justify-center">
     <div class="full-width q-pa-xl">
-      <h6 class="q-my-none q-mb-md">Deleted Tasks</h6>
+      <h6 class="q-my-none q-mb-md">My Deleted Tasks</h6>
       <div class="q-mt-xl">
         <!-- The <q-card> element represents a card element, and the v-for directive is a Vue.js directive that renders the card element multiple times based on an array. In this case, the card element will be rendered once for each item in the DeletedTasks array. The :key attribute is a directive that specifies a unique key for each rendered card element, and it is set to the id property of the row object. -->
         <q-card v-for="row in DeletedTasks" :key="row.id" class="q-mt-sm">
